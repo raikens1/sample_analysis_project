7c928d8754e3fde905f51460b7b7ba4dee6a4cd9
raikens1 <rockyaikens@gmail.com>
2020-08-11 18:12:55 -0700

    delete documentation for generate data

--- R/generate_data.R
+++ R/generate_data.R
@@ -2,19 +2,11 @@
 # GENERATE UNDIAGNOSED DATA
 # ------------------------------------------------------------------------------
 
-#' Generate Cross Sectional Data
-#'
-#' @param n sample size
-#' @param prevalence disease prevalence
-#'
-#' @return data frame of n individuals baseline binary covariate x and desease
-#'   severity
-#' @export
 generate_cross_sectional <- function(n = 10000, prevalence = 0.15){
-  result <- data.frame(
-    x = (rbinom(n, 1, 0.5) == 1),
-    disease = rbinom(n, 1, prevalence)
-    ) %>%
+  # generate data with id, disease, severity
+  result <- data.frame(id = 1:n, disease = rbinom(n, 1, prevalence)) %>%
     dplyr::mutate(severity = disease * runif(n))
+  
+  return(result)
 }
 
